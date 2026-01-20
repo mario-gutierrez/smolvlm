278aa3ce7832a68fce59849e2cb10975992d3fe6
Mario Gutierrez <mgutierrez1@logitech.com>
2026-01-20 09:39:56 +0100

    Setup github pages

--- smolvlm-webgpu/vite.config.js
+++ smolvlm-webgpu/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const REPO_NAME ='/smolvlm/'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: REPO_NAME,
+  build: {
+    target: 'esnext' // WebGPU requires modern browser support
+  },
+  worker: {
+    format: 'es' // Ensure workers are bundled as ES modules
+  }
 });
